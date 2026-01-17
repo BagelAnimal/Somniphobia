1b212cf36f610f7d679d7a1c2ede7d0692554503
James Ward <esotericaow@gmail.com>
2026-01-17 17:40:56 -0500

    Added ThirdParty Folder, Small Game.cs Improvements & Notes Markdown File

--- U.Somniphobia/Assets/Somniphobia/Game.cs
+++ U.Somniphobia/Assets/Somniphobia/Game.cs
@@ -2,9 +2,17 @@ using UnityEngine;
 
 namespace FulcrumGames.Somniphobia
 {
+    /// <summary>
+    ///     The intent is to provide a common access point for game state,
+    ///     and to provide a top-most point for all code execution associated
+    ///     with the game. In order to spin up an instance of the game,
+    ///     at least one scene needs to be launched with an instance
+    ///     of this component attached to its GameObject.
+    /// </summary>
     public class Game : MonoBehaviour
     {
         private static Game s_instance;
+        public static Game Instance => s_instance;
 
         private void Awake()
         {
@@ -15,6 +23,7 @@ namespace FulcrumGames.Somniphobia
             }
 
             s_instance = this;
+            DontDestroyOnLoad(s_instance.gameObject);
         }
     }
 }
---
+++ somniphobia_notes.md
@@ -0,0 +1,5 @@
+Here, notes about the project and its goals will be captured.
+
+f5 can be used to attach VS Code to the editor instance.
+Launch MCP local session using window & `http://localhost:8080`.
+Need uv installed (can be installed via Powershell) to run MCP local session.
