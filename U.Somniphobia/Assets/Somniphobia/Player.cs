1608da2ef189990876915806a7b02c062200a38d
James Ward <esotericaow@gmail.com>
2026-01-17 16:48:26 -0500

    Retracted Existing Architecture (going with something a bit cleaner)

--- U.Somniphobia/Assets/Somniphobia/Player.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FulcrumGames.Somniphobia
-{
-    public sealed class Player
-    {
-        private InputActions _inputActions = new();
-        public InputActions InputActions => _inputActions;
-    }
-}
