5333923286acff968a3516f2194fbc6a681bafe9
James Ward <esotericaow@gmail.com>
2026-03-07 14:53:16 -0800

    Characters are No Longer Considered to be Walking if Providing a Vertical Input

--- U.Somniphobia/Assets/Somniphobia/CharacterControl/Walk.cs
+++ U.Somniphobia/Assets/Somniphobia/CharacterControl/Walk.cs
@@ -93,7 +93,7 @@ namespace FulcrumGames.CharacterControl
             var forward = _directionAnchor.forward;
             var right = _directionAnchor.right;
 
-            _isWalking = _input != Vector3.zero;
+            _isWalking = _input.x != 0.0f || _input.z != 0.0f;
             var desiredDirection = forward * _input.z + right * _input.x;
 
             var isGrounded = _groundDetector.IsGrounded;
