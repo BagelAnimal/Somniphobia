fbedc9cb554aa84f473e8034619616f68c723000
James Ward <esotericaow@gmail.com>
2026-03-07 14:49:13 -0800

    Added Rotation Velocity Clear to Look Rotation Copy

--- U.Somniphobia/Assets/Somniphobia/CharacterControl/Look.cs
+++ U.Somniphobia/Assets/Somniphobia/CharacterControl/Look.cs
@@ -126,6 +126,7 @@ namespace FulcrumGames.CharacterControl
             _yaw = otherLook._yaw;
             _roll = otherLook._roll;
 
+            _rotationVelocity = Vector3.zero;
             _targetRotationEuler = new Vector3(_pitch, _yaw, _roll);
 
             _pitchPivot.transform.localRotation = Quaternion.Euler(Vector3.right * _pitch);
